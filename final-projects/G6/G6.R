6a75877b1fcdf96028563c5232aaf176567ab46c
Martin老師 <tpemartin@gmail.com>
2023-12-20 11:10:03 +0800

    Update G6.R

--- final-projects/G6/G6.R
+++ final-projects/G6/G6.R
@@ -1,21 +1,71 @@
-# https://github.com/tpemartin/112-1-programming-for-data-science/issues/13#issuecomment-1842200202
-
 library(readr)
+dg6 <- readr::read_csv("https://data.cip.gov.tw/API/v1/dump/datastore/A53000000A-112010-003")
+View(dg6)
 
-# 111年1-12月娛樂稅稅源月報表UTF-8 ----
-Tax<- read_csv("https://raw.githubusercontent.com/Urifoz/112-1-final-project/main/111年1-12月娛樂稅稅源月報表UTF-8.csv")
-View(Tax)
-
-## Original
-Tax$...1
-result <- grep("月合計", Tax$...1)
-print(result)
-print(Tax$營業總額[result])
-
-## Suggestion
-Tax |>
+# 男女看報紙比例----
+## old ----
+if(FALSE)
+{
+dg6 |>
   dplyr::filter(
-    grepl("月合計", `...1`)
-  ) -> Tax_monthlyTotal
+    類別 == "性別"
+  ) -> dg6_1
 
-View(Tax_monthlyTotal)
+dg6_1 |>
+  split(
+    dg6_1$項目別
+  ) -> split_dg6_1
+
+split_dg6_1$女$看報紙/
+  split_dg6_1$男$看報紙 -> summarise1
+
+}
+## new ----
+## 若沒有tidyr則安裝
+if(!require(tidyr)) install.packages("tidyr")
+
+dg6 |>
+  dplyr::select(
+    年度, 類別,看報紙,項目別
+  ) |>
+  dplyr::filter(
+    類別 == "性別"
+  ) -> dg6_filtered
+
+head(dg6_filtered)
+
+dg6_filtered |>
+    tidyr::pivot_wider(
+    names_from = "項目別",
+    values_from = "看報紙"
+  )  |>
+  dplyr::mutate(
+      男_女比=男/女
+  )
+
+# others ----
+names(summarise1) <-
+  split_dg6_1$女$年度
+
+summarise1
+
+
+dg6 |>
+  dplyr::filter(
+    類別 == "年齡"
+  ) -> dg6_2
+
+View(dg6_2)
+
+dg6_2 |>
+  split(
+    dg6_2$項目別
+  ) -> split_dg6_2
+
+split_dg6_2$"15 ~ 19歲"$看報紙/
+  split_dg6_2$"65歲及以上"$看報紙 -> summarise2
+
+names(summarise2) <-
+  split_dg6_2$"15 ~ 19歲"$年度
+
+summarise2
