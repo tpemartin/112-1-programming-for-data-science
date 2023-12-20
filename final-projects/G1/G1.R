203acb7465afd20161f8b42b66b5b9e60474eeb3
Martin老師 <tpemartin@gmail.com>
2023-12-20 09:58:26 +0800

    g1

--- final-projects/G1/G1.R
+++ final-projects/G1/G1.R
@@ -1,17 +1,47 @@
+#前置作業----
 library(readr)
 library(dplyr)
+data <- jsonlite::fromJSON("data.json")
 
-# 兒童少年開戶資料兒童及少年未來教育與展帳戶-開戶率 ----
-data1 <- read_csv("https://raw.githubusercontent.com/angelaachen/112-1-final-project-G1/main/110年度兒童及少年未來教育與展帳戶-開戶率.csv",
-                  locale = locale(encoding = "BIG5"))
+#命名Values----
+on_stop <- data$on_stop
+off_stop <- data$off_stop
+sum_of_txn_times <- data$sum_of_txn_times
 
-#
-data <- jsonlite::fromJSON("https://raw.githubusercontent.com/tpemartin/112-1-programming-for-data-science/main/final-projects/G1/data.json")
+data_arranged <-
+  data |>
+  dplyr::arrange(on_stop) |>
+  dplyr::relocate(on_stop)
+View(data_arranged)
 
-class(data)
-on_stop_id <- data$on_stop_id
-class(on_stop_id)
-table(on_stop_id)
-data$on_stop_id[[527]]
+data_arranged |>
+  purrr::map(class)
 
-test <- dplyr::arrange(data,on_stop_id)
+#改變data$sum_of_txn_times屬性----
+data$sum_of_txn_times <- as.numeric(data$sum_of_txn_times)
+
+data |>
+  dplyr::mutate(
+    sum_of_txn_times = as.numeric(sum_of_txn_times)
+  ) -> data
+
+summaries <- list()
+#sum up交易次數by on/off stop----
+sum_of_txn_times_numbered_on_stop <-
+  data |> dplyr::group_by(on_stop) |>
+  dplyr::summarise(
+    sum_of_txn_times = sum(sum_of_txn_times)
+  )
+summaries$`sum up交易次數by on_stop` <- sum_of_txn_times_numbered_on_stop
+
+sum_of_txn_times_numbered_off_stop <-
+  data |> dplyr::group_by(off_stop) |>
+  dplyr::summarise(
+    sum_of_txn_times = sum(sum_of_txn_times)
+  )
+summaries$`sum up交易次數by off_stop` <- sum_of_txn_times_numbered_off_stop
+
+saveRDS(summaries, file="summaries.Rds")
+#gg----
+
+
