28c30db6d2a4d553ba743c6982fedcddf1082d15
Martin老師 <tpemartin@gmail.com>
2024-01-10 10:42:26 +0800

    update

--- final-project-management.R
+++ final-project-management.R
@@ -1,7 +1,8 @@
 googlesheets4::gs4_auth()
-ss="https://docs.google.com/spreadsheets/d/1Z7rTyOesELNgjst5zFU9uFEAqFqc3I7jB_1j3rKQe9c/edit?usp=sharing"
+ss <- "https://docs.google.com/spreadsheets/d/1Z7rTyOesELNgjst5zFU9uFEAqFqc3I7jB_1j3rKQe9c/edit?usp=sharing"
 googlesheets4::read_sheet(ss,
-                          range="分組名單!A1:F11") -> values
+  range = "分組名單!A1:F11"
+) -> values
 
 values |>
   tidyr::pivot_longer(
@@ -12,51 +13,54 @@ values |>
 
 googlesheets4::read_sheet(
   ss,
-  range="組員資訊!A1:D"
+  range = "組員資訊!A1:D"
 ) -> values2
 
 values2 |>
   dplyr::select(-`group name`) |>
   dplyr::left_join(
     values_long,
-    by=c("選單"='member')
+    by = c("選單" = "member")
   ) |>
   dplyr::relocate("group name") |>
   dplyr::mutate(
-    `Github username`=unlist(`Github username`)
-  )-> values_final
+    `Github username` = unlist(`Github username`)
+  ) -> values_final
 
 clipr::write_clip(
   values_final,
-    object_type = "table"
-  )
+  object_type = "table"
+)
 
 # Attendance grades
-ss="https://docs.google.com/spreadsheets/d/1kOkjekNjLcdJVvZ5zD3wx3gnY0L7gI1M8sK17xXwkr4/edit#gid=1233661760"
-googlesheets4::read_sheet(ss,
-                          "github"
-                          ) -> values
-googlesheets4::read_sheet(ss,
-                          "最終修課名單")-> roster
+ss <- "https://docs.google.com/spreadsheets/d/1kOkjekNjLcdJVvZ5zD3wx3gnY0L7gI1M8sK17xXwkr4/edit#gid=1233661760"
+googlesheets4::read_sheet(
+  ss,
+  "github"
+) -> values
+googlesheets4::read_sheet(
+  ss,
+  "最終修課名單"
+) -> roster
 values |>
   ## remove preparation weeks
-  dplyr::select(-c("2023-09-20","2023-09-27")) |>
+  dplyr::select(-c("2023-09-20", "2023-09-27")) |>
   ## make all attedance logical class
   dplyr::mutate(
     dplyr::across(
       `2023-10-11`:`2023-12-20`,
-      .fns=function(x) as.logical(x)
+      .fns = function(x) as.logical(x)
     )
   ) |>
   ## count number of attendance
   dplyr::rowwise() |>
   dplyr::mutate(
-    `總次數`=length(dplyr::c_across(`2023-10-11`:`2023-12-20`)),
-    `出席次數`=sum(dplyr::c_across(`2023-10-11`:`2023-12-20`), na.rm=T),
-  ## allow to drop one attendance
-    `有效總次數`=`總次數`-1,
-    `計分出席次數`=min(`出席次數`,`有效總次數`),
-    `出席分數`=round(`計分出席次數`/`有效總次數`*20,2)
+    `總次數` = length(dplyr::c_across(`2023-10-11`:`2023-12-20`)),
+    `出席次數` = sum(dplyr::c_across(`2023-10-11`:`2023-12-20`), na.rm = T),
+    ## allow to drop one attendance
+    `有效總次數` = `總次數` - 1,
+    `計分出席次數` = min(`出席次數`, `有效總次數`),
+    `出席分數` = round(`計分出席次數` / `有效總次數` * 20, 2)
   ) |>
   ## drop duplicated name
   dplyr::filter(
@@ -67,37 +71,43 @@ values |>
     roster |>
       dplyr::select(姓名, 學號) |>
       dplyr::mutate(
-        選單=paste0("(",stringr::str_sub(學號,-3),")",姓名)
+        選單 = paste0("(", stringr::str_sub(學號, -3), ")", 姓名)
       ),
-    by =c("(-學號末3碼）姓名"="選單")
+    by = c("(-學號末3碼）姓名" = "選單")
   ) |>
   dplyr::select(
     姓名, 學號, `出席次數`, `總次數`
-    ) |>
+  ) |>
   dplyr::arrange(
     學號
-    )-> attendance
+  ) -> attendance
 
-googlesheets4::write_sheet(attendance, ss=ss,
-                           "attendance")
+googlesheets4::write_sheet(attendance,
+  ss = ss,
+  "attendance"
+)
 
 ## Bonus
 groups <-
   googlesheets4::read_sheet(
     ss, "組員資訊"
   )
-googlesheets4::read_sheet(ss,
-                          "最終修課名單")-> roster
-googlesheets4::read_sheet(ss,
-                          "github"
+googlesheets4::read_sheet(
+  ss,
+  "最終修課名單"
+) -> roster
+googlesheets4::read_sheet(
+  ss,
+  "github"
 ) -> github
 roster |>
   dplyr::select(姓名, 學號) |>
   dplyr::mutate(
-    選單=paste0("(",stringr::str_sub(學號,-3),")",姓名)
+    選單 = paste0("(", stringr::str_sub(學號, -3), ")", 姓名)
   ) |>
   dplyr::left_join(
-    groups, by="選單"
+    groups,
+    by = "選單"
   ) -> roster
 
 ## Group bonus
@@ -113,16 +123,17 @@ bonus_group[is.na(bonus_group)] <- FALSE
 roster |>
   dplyr::left_join(
     bonus_group,
-    by=c("group name"="group")
+    by = c("group name" = "group")
   ) -> roster2
 
 # week14, 15 attendance
 github |>
   dplyr::select(
-    `(-學號末3碼）姓名`,`2023-12-13`,`2023-12-20`) |>
+    `(-學號末3碼）姓名`, `2023-12-13`, `2023-12-20`
+  ) |>
   dplyr::right_join(
     roster2,
-    by = c("(-學號末3碼）姓名"="選單")
+    by = c("(-學號末3碼）姓名" = "選單")
   ) |>
   dplyr::mutate(
     issue11_valid = issue11,
@@ -132,7 +143,7 @@ github |>
 
 roster_withGroupBonus |>
   dplyr::mutate(
-    `Github username`= as.character(`Github username`)
+    `Github username` = as.character(`Github username`)
   ) |>
   dplyr::mutate(
     issue8 = `Github username` %in% bonus_individual$issue8
@@ -143,21 +154,21 @@ roster_withAllBonus |>
     姓名, 學號, issue8, issue11_valid, issue13_valid, issue16_valid
   ) |>
   dplyr::mutate(
-    issue8_individual = issue8*2,
-    issue11_group = issue11_valid*1,
-    issue13_group = issue13_valid*1,
-    issue16_group = issue16_valid*1
+    issue8_individual = issue8 * 2,
+    issue11_group = issue11_valid * 1,
+    issue13_group = issue13_valid * 1,
+    issue16_group = issue16_valid * 1
   ) |>
   dplyr::select(
     姓名, 學號, issue8_individual, issue11_group, issue13_group, issue16_group
   ) |>
   dplyr::rowwise() |>
   dplyr::mutate(
-    學期加分=sum(dplyr::c_across(
+    學期加分 = sum(dplyr::c_across(
       issue8_individual:issue16_group
     ))
   ) |>
-  dplyr::arrange(學號)-> bonus_all
+  dplyr::arrange(學號) -> bonus_all
 
 bonus_all |>
   googlesheets4::write_sheet(
@@ -165,42 +176,237 @@ bonus_all |>
     "學期總成績加分"
   )
 
-# final project
+# final project -----
 source("support_final_project.R")
-ss="https://docs.google.com/spreadsheets/d/1kOkjekNjLcdJVvZ5zD3wx3gnY0L7gI1M8sK17xXwkr4/edit?pli=1#gid=1556663194"
-assessment = googlesheets4::read_sheet(ss, sheet="final-project-assessment")
-assessment |>
-  dplyr::filter(!(user %in% c("(007)Martin", "(008)Fabia")))
-roster = googlesheets4::read_sheet(ss, sheet="最終修課名單")
+ss <- "https://docs.google.com/spreadsheets/d/1kOkjekNjLcdJVvZ5zD3wx3gnY0L7gI1M8sK17xXwkr4/edit?pli=1#gid=1556663194"
+assessment_all <- googlesheets4::read_sheet(ss, sheet = "final-project-assessment")
+assessment_all |>
+  dplyr::filter(!(user %in% c("(007)Martin", "(008)Fabia"))) -> assessment
+roster <- googlesheets4::read_sheet(ss, sheet = "最終修課名單")
 roster$學號 |>
   stringr::str_sub(-3, -1) -> id3digits
 namelist <- paste0("(", id3digits, ")", roster$姓名)
-grouplist <- paste0("G",1:11)
-project_grades = vector("list", 11) |>
-  setNames(grouplist)
-member_grades = vector("list",length(namelist)) |>
-  setNames(namelist)
+roster$namelist <- namelist
+
+studentAssessments <- compute_student_assessment(assessment)
+
+groupSheet <- googlesheets4::read_sheet(
+  ss, "組員資訊"
+)
+roster |>
+  dplyr::left_join(
+    groupSheet,
+    by = c("namelist" = "選單")
+  ) -> roster
+
+teacherAssessments <-
+  assessment_all |>
+  dplyr::filter(
+    user=="(007)Martin"
+  )
+
+finalProjectGrade <- merge_assessments(studentAssessments, teacherAssessments)
+
+# special case
+finalProjectGrade |>
+  split(
+    finalProjectGrade$group.name=="G9"
+  ) -> split_finalProjectGrade
+df <- split_finalProjectGrade$`TRUE`
+split_finalProjectGrade$`TRUE`[
+  df$namelist=="(087)陳雲凱",
+  "member_grade_byStudents"
+] <- 5
+
+split_finalProjectGrade$`TRUE`[
+  df$namelist!="(087)陳雲凱",
+  c("project_grade_byTeacher","project_grade_byStudents")
+] <- 0
+
+finalProjectGrade <- unsplit(
+  split_finalProjectGrade,
+  finalProjectGrade$group.name=="G9"
+)
+
+finalProjectGrade |>
+  dplyr::select(
+    group.name, namelist, 學號, member_grade_byStudents,
+    project_grade_byStudents, project_grade_byTeacher
+  ) -> finalProjectGrade
+
+finalProjectGrade |>
+  dplyr::mutate(
+    finalProject = 10*(member_grade_byStudents/5)+
+      10*(project_grade_byStudents/5)+
+      20*(project_grade_byTeacher/5)
+  ) -> finalProjectGrade
+
+finalProjectGrade[is.na(finalProjectGrade)] <- 0
+
+View(finalProjectGrade)
+
+finalProjectGrade |>
+  googlesheets4::write_sheet(
+    ss, "final project grades"
+  )
+
+# final project assessment bonus -----
+check_assessmentNotNull <- function(jsonassessment) {
+  jsonassessment |>
+    jsonlite::fromJSON() |>
+    unlist() |>
+    is.null() -> noBonus
+  !noBonus
+}
 
 assessment$`project grades` |>
-  purrr::reduce(.f=function(pg, .x){
-    pg |>
-      record_projectGradesJson(.x, grouplist)
-  }, .init=project_grades) -> project_grades
+  purrr::map_lgl(
+    check_assessmentNotNull
+  ) |>
+  as.integer() -> assessment$參與作品評分
 
 assessment$`member grades` |>
-  purrr::reduce(.f=function(pg, .x){
-    pg |>
-      record_memberGradesJson(.x, namelist)
-  }, .init=member_grades) -> member_grades
+  purrr::map_lgl(
+    check_assessmentNotNull
+  ) |>
+  as.integer() -> assessment$參與組員互評
+
+googlesheets4::read_sheet(
+  ss, "學期總成績加分"
+) -> semesterBonus
+
+assessment |>
+  dplyr::select(user,參與作品評分,參與組員互評) |>
+  dplyr::right_join(
+    roster |>
+      dplyr::select(namelist, 學號),
+    by=c("user"="namelist")
+  ) |>
+  dplyr::right_join(
+    semesterBonus,
+    by="學號"
+  ) -> semesterBonus
+
+names(semesterBonus)
+
+semesterBonus[,
+              c(
+                "姓名", "學號", "issue8_individual", "issue11_group", "issue13_group",
+                "issue16_group", "參與作品評分", "參與組員互評", "學期加分")] ->
+  semesterBonusFinal
+semesterBonusFinal |>
+  dplyr::rowwise() |>
+  dplyr::mutate(
+    學期加分=sum(dplyr::c_across(issue8_individual:參與組員互評))
+  ) -> semesterBonusFinal
+semesterBonusFinal[is.na(semesterBonusFinal)] <-0
+googlesheets4::write_sheet(
+  semesterBonusFinal,
+  ss, "學期總成績加分"
+)
+
+googlesheets4::read_sheet(
+  ss,
+  "attendance"
+) -> attendance
+
+attendance |>
+  dplyr::mutate(
+    學期出席成績=20* pmin(出席次數,7)/7
+  ) -> attendanceFinal
+
+googlesheets4::write_sheet(
+  attendanceFinal,
+  ss,
+  "attendance"
+)
+
+# Semester grade ----
+ss <- "https://docs.google.com/spreadsheets/d/1kOkjekNjLcdJVvZ5zD3wx3gnY0L7gI1M8sK17xXwkr4/edit#gid=1868367100"
+
+googlesheets4::read_sheet(
+  ss,
+  "Midterm-上傳"
+) -> midterm
+googlesheets4::read_sheet(
+  ss,
+  "final project grades"
+) -> finalProject
+googlesheets4::read_sheet(
+  ss,
+  "attendance"
+) -> attendance
+googlesheets4::read_sheet(
+  ss,
+  "學期總成績加分"
+) -> bonus
+
+roster <- googlesheets4::read_sheet(ss, sheet = "最終修課名單")
+roster$學號 |>
+  stringr::str_sub(-3, -1) -> id3digits
+namelist <- paste0("(", id3digits, ")", roster$姓名)
+roster$namelist <- namelist
+
+library(dplyr)
+midterm |>
+  select(代號,Total) |>
+  rename(
+    "學號"="代號",
+    "期中考(原始分數, 滿分100)"="Total"
+  ) |>
+  mutate(
+    `期中考學期分數(滿分40)`=`期中考(原始分數, 滿分100)`/100*40
+  ) -> midtermFinal
+finalProject |>
+  select(學號, finalProject) |>
+  rename(
+    "期末專期學期分數(滿分40)"="finalProject"
+  ) -> finalProjectFinal
+attendance |>
+  select(學號,學期出席成績) |>
+  rename(
+    "學期出席成績(滿分20)"="學期出席成績"
+  ) -> attendanceFinal
+roster |>
+  left_join(
+    midtermFinal,
+    by="學號"
+  ) |>
+  left_join(
+    finalProjectFinal,
+    by="學號"
+  ) |>
+  left_join(
+    attendanceFinal,
+    by="學號"
+  ) |>
+  mutate(
+    "學期總成績(不含加分)"=`期中考學期分數(滿分40)`+
+      `期末專期學期分數(滿分40)`+
+      `學期出席成績(滿分20)`
+  ) |>
+  select(
+    學號,`期中考學期分數(滿分40)`,
+      `期末專期學期分數(滿分40)`,
+      `學期出席成績(滿分20)`,
+    `學期總成績(不含加分)`
+  ) -> semesterFinal
+
+bonus |>
+  select(姓名,學號,學期加分) -> bonusFinal
+
+semesterFinal |>
+  left_join(
+    bonusFinal,
+    by = "學號"
+  ) |>
+  relocate("姓名")-> semesterFinal2
 
 
-project_grades |>
-  purrr::map(function(.x) mean(.x, na.rm=T)) ->
-  project_grades_studentAssessment
+semesterFinal2[is.na(semesterFinal2)] <- 0
 
-member_grades |>
-  purrr::map(function(.x){
-    if(is.null(.x)) return(0)
-    mean(as.numeric(.x))
-  }) ->
-  member_grades_studentAssessment
+googlesheets4::write_sheet(
+  semesterFinal2,
+  ss,
+  "學期成績"
+)
--- support_final_project.R
+++ support_final_project.R
@@ -1,3 +1,79 @@
+
+merge_assessments <- function(studentAssessments, teacherAssessments){
+  {
+    data.frame(
+      `group name` = studentAssessments$project_grades_studentAssessment |> names(),
+      `project_grade_byStudents` = unlist(studentAssessments$project_grades_studentAssessment)
+    ) |>
+      dplyr::right_join(
+        roster,
+        by = c("group.name" = "group name")
+      ) -> finalProject
+  }
+  {
+    data.frame(
+      `namelist` = studentAssessments$member_grades_studentAssessment |> names(),
+      `member_grade_byStudents` = unlist(studentAssessments$member_grades_studentAssessment)
+    ) |>
+      dplyr::right_join(
+        finalProject,
+        by = c("namelist" = "namelist")
+      ) -> finalProject
+  }
+  {
+
+    teacherAssessments$`project grades`[[1]] |>
+      jsonlite::fromJSON() -> pg
+    pg = unlist(pg)
+    groupNames = names(pg)
+    data.frame(
+      `group name` =  groupNames,
+      `project_grade_byTeacher` = unlist(pg[groupNames])
+    ) |>
+      dplyr::right_join(
+        finalProject,
+        by = c("group.name" = "group.name")
+      ) -> finalProject
+  }
+  return(finalProject)
+}
+compute_student_assessment <- function(assessment) {
+  grouplist <- paste0("G", 1:11)
+  project_grades <- vector("list", 11) |>
+    setNames(grouplist)
+  member_grades <- vector("list", length(namelist)) |>
+    setNames(namelist)
+
+  assessment$`project grades` |>
+    purrr::reduce(.f = function(pg, .x) {
+      pg |>
+        record_projectGradesJson(.x, grouplist)
+    }, .init = project_grades) -> project_grades
+
+  assessment$`member grades` |>
+    purrr::reduce(.f = function(pg, .x) {
+      pg |>
+        record_memberGradesJson(.x, namelist)
+    }, .init = member_grades) -> member_grades
+
+
+  project_grades |>
+    purrr::map(function(.x) mean(.x, na.rm = T)) ->
+    project_grades_studentAssessment
+
+  member_grades |>
+    purrr::map(function(.x) {
+      if (is.null(.x)) {
+        return(0)
+      }
+      mean(as.numeric(.x))
+    }) ->
+    member_grades_studentAssessment
+  return (list(
+    project_grades_studentAssessment=project_grades_studentAssessment,
+    member_grades_studentAssessment=member_grades_studentAssessment
+  ))
+}
 record_projectGradesJson <- function(project_grades, projectGradesJson, grouplist) {
   # projectGradesJson <- assessment$`project grades`[[1]]
   jsonlite::fromJSON(projectGradesJson) -> projectGrades
